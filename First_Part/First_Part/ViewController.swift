a4021b714f050eb9b202289664673650688b3ef9
Pavel71 <raz_3@mail.ru>
2019-02-28 09:45:46 +0300

    Init First Part

---
+++ First_Part/First_Part/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  First_Part
+//
+//  Created by PavelM on 28/02/2019.
+//  Copyright © 2019 PavelM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
